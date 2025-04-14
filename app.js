a7ed8fa6ffe24fa50a5a6957efabe37d7caa554e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-14 22:59:06 +0300

    feat: Basic security setup using suitable middlewares. Rate limiter for preventing the abuse of the api was added.

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const hpp = require("hpp");
+const morgan = require("morgan");
 const globalErrorController = require("./controllers/globalError.controller");
+const limiter = require("./middlewares/rateLimiter");
 const ApiError = require("./utils/apiError");
 
 const app = express();
+// Use helmet for setting security headers
+app.use(
+  helmet({
+    contentSecurityPolicy: false, // It is a restful API, so we dont need them
+  })
+);
+
+// Configure the allowed origins for this backend API
+app.use(
+  cors({
+    origin: ["http:127.0.0.1:5173"],
+    credentials: true,
+  })
+);
+
+// Rate limiter Middleware to limit repeated requests to our exposed endpoints
+app.use("/", limiter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Logging HTTP requests in Console in development
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Handle all unimplemented routes
 app.use("/{*splat}", (req, res, next) => {
